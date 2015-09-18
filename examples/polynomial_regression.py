fc19ba95db214411a6e91f88f67f7ad164ceb4be
Matheus Santos <matheus.cesario.santos@gmail.com>
2015-09-17 23:44:43 -0300

    Polynomial regression example

--- examples/polynomial_regression.py
+++ examples/polynomial_regression.py
@@ -1,32 +1,31 @@
 from linear_regression.LinearRegression import LinearRegression
 import matplotlib.pyplot as plt
 
-# Dataset
-# X = [ [2104, 5, 1, 45], [1416, 3, 2, 40], [1534, 3, 2, 30], [852,  2, 1, 36]]
-# Y = [460, 232, 315, 178]
-
-X = [[1], [2], [4], [8], [16], [32], [64]]
-Y = [1, 2, 3, 4, 5, 6, 7]
+X = [[1], [2], [3], [4], [5], [6], [7]]
+Y = [1, 2, 4, 8, 16, 32, 64]
 
 
 # Main program
 def main():
 
+    # Modeling dataset by adding x2=x1^2 and x3=x1^3
+    # so that y(x) = ax1^3 + bx1^2 + cx1 + d (polynomial curve)
+    X2 = [x + [x[0]**2, x[0]**3] for x in X]
+
     # Minimizing using normal equation method
-    lr = LinearRegression(X, Y)
+    lr = LinearRegression(X2, Y)
     lr.normalEquation()
-    predicted_Y1 = [lr.predict(x) for x in X]
+    predicted_Y1 = [lr.predict(x) for x in X2]
 
     # Minimizing using gradient descent method
-    lr = LinearRegression(X, Y)
-    lr.setAlpha(0.001)
+    lr = LinearRegression(X2, Y)
+    lr.setAlpha(0.000001)
     lr.gradientDescent()
-    predicted_Y2 = [lr.predict(x) for x in X]
-    print(lr.getErrorRate())
+    predicted_Y2 = [lr.predict(x) for x in X2]
 
     # Plotting example (2d graph)
     plt.plot(X, Y, 'o')  # Dots are original distribution
-    plt.plot(X, predicted_Y1, 'r')  # Red line is Normal equation
+    plt.plot(X, predicted_Y1, 'r')  # Red line is Gradient descent
     plt.plot(X, predicted_Y2, 'g')  # Green line is Gradient descent
     plt.show()
 
